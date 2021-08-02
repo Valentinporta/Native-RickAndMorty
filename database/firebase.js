a99c7dc725d7156c497d36860052bbf6a957c634
Valentin Porta <valentinporta93@gmail.com>
2021-08-02 10:53:48 -0300

    Added firebase component with config and init

---
+++ database/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase'
+
+// With this we can access the Firebase auth services via its API
+const firebaseConfig = {
+    apiKey: "AIzaSyCh8oQHG9il36hDNvqEnFELQpoC9idWGps",
+    authDomain: "rick-and-morty-native.firebaseapp.com",
+    projectId: "rick-and-morty-native",
+    storageBucket: "rick-and-morty-native.appspot.com",
+    messagingSenderId: "422257364045",
+    appId: "1:422257364045:web:465ac7f9193b2f5656e004"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase
